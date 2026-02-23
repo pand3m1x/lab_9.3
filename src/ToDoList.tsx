c5856b8e8478e98526f785522e401f087df921ae
TheRedQueen <pand3m1x@gmail.com>
2026-02-23 18:16:47 -0500

    ⋆⁺₊❅。Finished setting up visual representation (basic, very basic need to fix styling), moving on to TaskList ⋆⁺₊❅。

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,12 +1,51 @@
-// import React, { useState } from 'react'
+
 // import TaskList from './components/TaskList'
 
+  // id: string;
+  // title: string;
+  // description: string;
+  // status: TaskStatus;
+  // priority: 'low' | 'medium' | 'high';
+  // dueDate: string;
+import { useState } from "react";
+import type { Task } from "./types/index.ts";
+
 function ToDoList(){
+
+  const [tasks, setTasks] = useState <Task[]>([
+      {id: "1",
+       title: "Take over the WORLD!",
+       description: "The same thing we do every night, Pinky",
+       status: "pending",
+       priority: "high",
+       dueDate: "2026-02-23",
+
+      },
+
+       {id: "2",
+       title: "Reheat Revenge",
+       description: "Utilizing the Microwave of Evil",
+       status: "completed",
+       priority: "high",
+       dueDate: "2010-11-05",
+
+      }
+
+  ]);
   
   return(
-    <div id="taskCard" style={{width:"600px", backgroundColor:"gray", borderRadius:"10px", padding:"20px"}}>
+    <div id="taskCard" style={{width:"600px", backgroundColor:"lightGray", borderRadius:"10px", padding:"20px", borderStyle:"2px solid grey"}}>
       <h1 style={{alignContent:"center"}}>Task List</h1>
-      
+      <ul style={{listStyleType:"none"}}> {tasks.map(task =>(
+        <li key={task.id} style={{backgroundColor:"white", borderRadius:"10px", width:"500px", borderStyle:"2px solid black"}}>
+          <strong>{task.title}</strong>
+          <p>{task.description}</p>
+          <p>{task.priority}</p>
+          <p>{task.status}</p>
+          <button style={{backgroundColor:"red", width:"50px", fontSize:".8rem"}}>delete</button>
+        </li>
+      ))}
+      </ul>
     </div>
   );
 }
